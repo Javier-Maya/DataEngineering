49662dd37a303846e319ded2bc4a141b51b144fe
Javier Maya <j.mayanavarrete@gmail.com>
2023-10-03 23:45:03 -0300

    Segunda preentrega

--- pokemonAPI.py
+++ pokemonAPI.py
@@ -1,15 +1,33 @@
 import requests
+import psycopg2
+import json
+import os
+from dotenv import load_dotenv
+from psycopg2 import Error
+import pandas as pd
 
-# pokemon_name = "CHARMANDER"
-pokemon_name = input("Ingresa nombre de Pokémon a buscar: ").lower()
-url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}/"
+# URL de la API de Pokémon para obtener información del Pokémon
+pokemon_api_url = 'https://pokeapi.co/api/v2/pokemon/'
 
-response = requests.get(url)
+# Conectándose a la base de datos Redshift
+try:
+    load_dotenv()
+    connection = psycopg2.connect(
+        user='j_mayanavarrete_coderhouse',
+        password=os.getenv('PASSWORD'),
+        host=os.getenv('HOST'),
+        port=int(os.getenv('PORT')),
+        database=os.getenv('DATABASE')
+    )
 
-if response.status_code == 200:
+    cursor = connection.cursor()
+
+    # Solicitud a la API de Pokémon para obtener información del Pokémon
+    pokemon_name = input("Ingresa nombre de Pokémon a buscar: ").lower()
+    response = requests.get(pokemon_api_url + str(pokemon_name))
     pokemon_data = response.json()
-    
-    # Extraer información relevante del Pokémon
+
+    # Extraer la información necesaria del Pokémon
     id = pokemon_data['id']
     name = pokemon_data['name']
     height = pokemon_data['height']
@@ -31,14 +49,37 @@ if response.status_code == 200:
         weaknesses = [t['name'] for t in weaknesses_data['damage_relations']['double_damage_from']]
     else:
         weaknesses = []
-    
-    # Mostrar la información por pantalla
-    print(f"ID: {id}")
-    print(f"Nombre: {name}")
-    print(f"Altura: {height / 10} metros")
-    print(f"Peso: {weight / 10} kilogramos")
-    print(f"Tipo(s): {', '.join(types)}")
-    print(f"Top {moves_limit} movimientos: {', '.join(moves)}")
-    print(f"Debilidad(es): {', '.join(weaknesses)}")
-else:
-    print("No se pudo obtener la información del Pokémon.")
+
+    # Convierte las listas a objetos JSON
+    types_json = json.dumps(types)
+    moves_json = json.dumps(moves)
+    weaknesses_json = json.dumps(weaknesses)
+
+    # Leer datos existentes desde la base de datos
+    select_query = "SELECT * FROM pokemon"
+    cursor.execute(select_query)
+    existing_data = cursor.fetchall()
+
+    # Convertir los datos existentes en un DataFrame de Pandas
+    existing_df = pd.DataFrame(existing_data, columns=['id', 'nombre', 'altura', 'peso', 'tipo', 'movimientos', 'debilidades'])
+
+    # Comprobar si el nuevo Pokémon ya existe en la base de datos
+    if name.lower() in existing_df['nombre'].str.lower().values:
+        print(f"El Pokémon {name} ya existe en la base de datos. No se insertará el duplicado.")
+    else:
+        # Insertar el registro del Pokémon en la tabla de base de datos Redshift
+        insert_query = "INSERT INTO pokemon (id, nombre, altura, peso, tipo, movimientos, debilidades) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(insert_query, (id, name, height, weight, types_json, moves_json, weaknesses_json))
+        connection.commit()
+
+        print(f"Registro del Pokémon {name} insertado exitosamente en la tabla.")
+
+except Error as e:
+    print("Error al conectar a la base de datos Redshift:", e)
+
+finally:
+    # Cerrar la conexión a la base de datos
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Conexión a la base de datos cerrada.")
